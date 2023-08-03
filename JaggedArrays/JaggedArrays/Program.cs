eb83640320384ec0fb73ec7e9b6ab081e4ee1083
ktlhape <ktlhape@iie.ac.za>
2023-08-03 11:42:23 +0200

    Jagged Arrays to store hobbies of students. Jagged Arrays stores other arrays of the same data type

---
+++ JaggedArrays/JaggedArrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] jaggedArray = new string[4][];
+
+            string[] h_Kabelo = new string[] { "Tennis", "Soccer", "Chess" };
+            string[] h_Carol = new string[2];
+            string[] h_James = new string[] { "Swimming","Tennis","Hockey","Movies"};
+
+            jaggedArray[0] = h_Kabelo;
+            jaggedArray[1] = h_James;
+            jaggedArray[2] = h_Carol;
+            jaggedArray[3] = new string[] { "Boxing","Gym","Swimming","Dance"};
+
+            //Carol likes Swimming and dance
+
+            jaggedArray[2][0] = "Dance";
+            jaggedArray[2][1] = "Swimming";
+
+            //Display all the elements of the Jagged Array
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + ", ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
